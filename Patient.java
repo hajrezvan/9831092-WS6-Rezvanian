6991dc8ff870c6d6b8174d8dc37da9f091861df5
Haj-Rezvan <58388933+hajrezvan@users.noreply.github.com>
2020-11-11 15:51:05 +0330

    Implemented.

--- Patient.java
+++ Patient.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class Patient {
+    private MedicalRecord medicalRecord;
+    private boolean permission;
+    private ArrayList<Sickness> sicknesses;
+
+    public boolean isPermission() {
+        return permission;
+    }
+
+    public void setPermission(boolean permission) {
+        this.permission = permission;
+    }
+
+    public MedicalRecord getMedicalRecord() {
+        return medicalRecord;
+    }
+
+    public ArrayList<Sickness> getSicknesses() {
+        return sicknesses;
+    }
+
+    public void addSickness(Sickness sickness) {
+        sicknesses.add(sickness);
+    }
 }
